63a025a2383a8130510ae6933fd2bf865dc5c69b
gmaccario <g_maccario@hotmail.com>
2019-10-12 15:36:25 +0200

    Import Vue

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,4 +1,58 @@
-jQuery( document ).ready(function() {
-	
-	console.log( "We're going to play Battleship!" );
+/* *****************************************************************************************
+ *  
+ *  Main Game Object
+ *  
+ */
+const Game = Vue.component('game',{
+	components: {
+		
+	},
+	props: {
+		
+	},
+	data(){
+		return {}
+	},
+	created() {
+		
+	},
+	mounted() {
+
+	},
+	watch: {
+		
+	},
+	methods: {
+
+	},
+  	template:`<div>Vuejs Integration Board 1 and 2</div>`
+});
+
+/* *****************************************************************************************
+ *  
+ *  Main Vue Object
+ *  
+ */
+const vm = new Vue({
+    el: '#app',
+    components: {
+        'game': Game
+    },
+    data(){
+		return {
+			
+		}
+	},
+    created(){
+    	//this.setAuthCookie();
+	},
+    methods: {
+    	setAuthCookie(){
+    		/**
+             * @note Save a cookie and use it instead of the input value. 
+             * @todo Set expires=Mon, 03 Oct 2019 20:47:11 UTC; 
+             */    		
+       		document.cookie = `token=${this.token}; path=/`;
+    	}
+	}
 });
--- src/templates/index.php
+++ src/templates/index.php
@@ -29,6 +29,18 @@
                 </div>
             </div>
             
+            <div class="container-fluid">
+                <div class="row">
+                	<div class="col">
+                		<p></p>
+                		
+                    	<div id="app">
+                    		<game></game>
+                    	</div>
+                    </div>
+                </div>
+            </div>
+            
 			<div class="container-fluid">
 				<div class="row">
                     <div class="col">
@@ -47,6 +59,9 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         
+        <script type="text/javascript" src="https://unpkg.com/axios/dist/axios.min.js"></script>
+		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+		
         <script src="/src/public/js/main.js"></script>
     </body>
 </html>
