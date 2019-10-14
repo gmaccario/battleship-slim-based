fe79babdce6c2c9c55796d61554638fbd3324888
gmaccario <g_maccario@hotmail.com>
2019-10-14 11:49:50 +0200

    Created Separated Components for Ship and Hull

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,10 +1,154 @@
 // Event EventBus
 const EventBus = new Vue();
 
+// Cell Component
+const Cell = Vue.component('cell',{
+	components: {
+		
+	},
+	props: {
+		cols: Number,
+		col: Number,
+		row: Number,
+		alphabet: Array,
+		player: String,
+		token: String,
+	},
+	data(){
+		return {
+			hit: false,
+			attacked: false,
+		}
+	},
+	created() {
+		
+	},
+	mounted() {
+
+	},
+	watch: {
+
+	},
+	methods: {
+		
+		hitCoordinates(row, col) {
+			
+			//console.log("row", this.row, "col", this.col, 'player', this.player);
+			
+			if(!this.attacked)
+			{
+				axios.get('/api/hit-coordinates/player' + this.player + '/' + this.row + '/' + this.col, {
+	    			
+	    			headers: { Authorization: `${this.token}` }
+	    		
+				}).then((response) => {
+
+					console.log('hitCoordinates', response.data.results);
+					
+					if(!response.data.results.hit)
+					{
+						this.attacked = true;
+					}
+					else {
+						this.attacked = true;
+						this.hit = true;
+						
+						//console.log('hitShip', response.data.results.shipId);
+						this.$root.$emit('hitShip', response.data.results.shipId);
+						//EventBus.$emit('hitShip', response.data.results.shipId);
+					}
+				});	
+			}
+		}
+	},
+  	template:`<div @click="hitCoordinates((row - 1), (col - 1));">
+        		<i v-if="!attacked && !hit" class="fas fa-align-justify"></i>
+        		<i v-if="attacked && hit" class="fas fa-bomb"></i>
+        		<i v-if="attacked && !hit" class="fas fa-water"></i>
+        		
+        		<!-- Cell {{ alphabet[row - 1].toUpperCase() }}{{ (col - 1) }} -->
+        	</div>`
+});
+
+// Hull Component
+const Hull = Vue.component('hull',{
+	components: {
+		
+	},
+	props: {
+		shipId: Number,
+		hullId: Number,
+	},
+	data(){
+		return {
+			
+		}
+	},
+	created() {
+		
+	},
+	mounted() {
+		
+	},
+	watch: {
+		
+	},
+	methods: {
+		
+	},
+  	template:`<div class="ship-hull" :class="'hullId-' + hullId"></div>`
+});
+
+// Ship Component
+const Ship = Vue.component('ship',{
+	components: {
+		'hull': Hull,
+	},
+	props: {
+		ship: Object,
+	},
+	data(){
+		return {
+			id: 0,
+			//hulls: []
+		}
+	},
+	created() {
+		
+		this.setup();
+	},
+	mounted() {
+		
+		this.$root.$on('hitShip', (shipId) => {
+			
+			if(shipId == this.id) 
+			{
+				//console.log(shipId);
+				// @todo Make it red
+			}
+		})
+	},
+	watch: {
+		
+	},
+	methods: {
+		
+		setup(){
+			
+			this.id = this.ship.id;
+		}
+	},
+  	template:`<div class="ship" :class="'ship-' + ship.id + ' type-' + ship.type.split(' ').join('-').toLowerCase()">
+  				<div v-for="hullId in ship.len">
+  					<hull :shipId="ship.id" :hullId="hullId"></hull>
+  				</div>
+  			</diV>`
+});
+
 // Board Component
 const Board = Vue.component('board',{
 	components: {
-		
+		'cell': Cell,
 	},
 	props: {
 		player: String,
@@ -21,26 +165,13 @@ const Board = Vue.component('board',{
 		
 	},
 	mounted() {
-
+		
 	},
 	watch: {
 
 	},
 	methods: {
 
-		hitCoordinates(row, col) {
-			
-			console.log("row", row, "col", col, 'player', this.player);
-			
-			axios.get('/api/hit-coordinates/player' + this.player + '/' + row + '/' + col, {
-    			
-    			headers: { Authorization: `${this.token}` }
-    		
-			}).then((response) => {
-
-				console.log('hitCoordinates', response.data.results);
-			});
-		}
 	},
   	template:`<div class="board" :class="'player' + player">
   		<p>Board {{ player }}</p>
@@ -48,17 +179,20 @@ const Board = Vue.component('board',{
   		<table class="board" :class="'player' + player">
 		    <tbody>
 		        <tr v-for="row in rows" class="board-row" :class="'board-row-' + (row - 1)">
+		        
 		        	<td class="board-row-header" :class="'board-row-header-' + (row - 1)">{{ alphabet[row - 1].toUpperCase() }}</td>
-		        	<td v-for="col in cols" class="board-cell" :class="'board-cell-row-' + (row - 1) + ' board-cell-col-' + (col - 1)" @click="hitCoordinates((row - 1), (col - 1));">
-		        		<i class="fas fa-align-justify"></i>
-		        		<!-- Cell {{ alphabet[row - 1].toUpperCase() }}{{ (col - 1) }} -->
-		        	</td>
+		        	
+		        	<td v-for="col in cols" class="board-cell" :class="'board-cell-row-' + (row - 1) + ' board-cell-col-' + (col - 1)">
+	        			<cell :token="token" :player="player" :cols="cols" :col="col" :row="row" :alphabet="alphabet"></cell>
+	        		</td>
 		        </tr>
-		        <tr class="board-row-footer">
+		    </tbody>
+		    <tfoot>
+		    	<tr class="board-row-footer">
 		        	<td class="board-empty"><span>&nbsp;</span></td>
   					<td v-for="col in cols" class="board-col" :class="'board-col' + (col - 1)">{{ (col - 1) }}</td>
 		        </tr>
-		    </tbody>
+		    </tfoot>
 		  </table>
   		</div>`
 });
@@ -66,7 +200,7 @@ const Board = Vue.component('board',{
 // Fleet Component
 const Fleet = Vue.component('fleet',{
 	components: {
-		
+		'ship': Ship,
 	},
 	props: {
 		player: String,
@@ -108,9 +242,9 @@ const Fleet = Vue.component('fleet',{
   	template:`<div class="fleet" :class="'player' + player">
   			<p>Fleet {{ player }}</p>
   			
-  			<div class="ship" :class="'ship-' + ship.id + ' type-' + ship.type.split(' ').join('-').toLowerCase()" v-for="ship in ships">
-  				<div class="ship-hull" v-for="i in ship.len"></div>
-  			</diV> 
+  			<div v-for="ship in ships">
+	  			<ship :ship="ship"></ship>
+	  		</div>
   			 
   		</div>`
 });
